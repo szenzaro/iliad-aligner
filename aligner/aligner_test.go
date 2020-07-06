ef66ab2360c1256c3221088d07b07f037d8368be
Simone Zenzaro <s.zenzaro@gmail.com>
2020-07-06 10:28:13 +0200

    Add test for trie WIP

--- aligner/aligner_test.go
+++ aligner/aligner_test.go
@@ -1,6 +1,8 @@
 package aligner
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -13,4 +15,26 @@ func TestInclude(t *testing.T) {
 	if !a.includes(&Ins{W: w}) {
 		t.Error("fail")
 	}
+}
+
+func TestTrie(t *testing.T) {
+	sch, err := LoadScholie("../data/scholied.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	s := "ἡρώων"
+	s = "Πηληιάδεω"
+	// fmt.Println(sch.Find(s))
+	// fmt.Println(sch.Find(normalizeText(s)))
+	text := strings.ToLower(normalizeText(s))
+	fmt.Println(sch.Find(sch.PrefixSearch(text)[0]))
+	fmt.Println(sch.Find(sch.FuzzySearch(text)[0]))
+
+	for _, v := range sch.PrefixSearch(text) {
+		if x, ok := sch.Find(v); ok {
+			fmt.Println(x.Meta().([]string))
+		}
+	}
+
 }
